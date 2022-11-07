e4cad8496db68d021335ca1f748e2739af6b0cfc
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-11-07 15:47:53 +0300

    Deleted LocalStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,33 +1,7 @@
-import storage from 'redux-persist/lib/storage'
 import { rootReducer } from './rootReducer'
-import persistReducer from 'redux-persist/es/persistReducer'
 import { configureStore } from '@reduxjs/toolkit'
-import persistStore from 'redux-persist/es/persistStore'
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  whitelist: ['contacts'],
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: rootReducer,
 });
 
-export const persistor = persistStore(store);
